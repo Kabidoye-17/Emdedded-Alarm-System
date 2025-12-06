4c1af08023d54d0c013503407821805fc16b65ae
Kabidoye-17 <23365412@studentmail.ul.ie>
2025-12-06 14:48:02 +0000

    add non computer specific files to git

---
+++ Alarm-Workspace/m4/soc_init.h
@@ -0,0 +1,22 @@
+/**
+ * Configuration for MAX32655-CTBGA.
+ *
+ * This file was generated using Analog Devices CodeFusion Studio.
+ * https://github.com/analogdevicesinc/codefusion-studio
+ *
+ * Generated at: 2025-12-05T17:57:02.791Z *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Analog Devices, Inc.
+ */
+
+
+#ifndef SOC_INIT_H
+#define SOC_INIT_H
+
+
+/* Prototypes for functions in the corresponding SoC init file.
+*/
+int PinInit(void);
+int PeripheralInit(void);
+
+#endif
