1ca20a29ebf74f01f19c1becea132b27ff865920
ElliceNels <23371323@studentmail.ul.ie>
2025-12-08 17:03:55 +0000

    [Feat] Add template c file functions

---
+++ Alarm-Workspace/m4/src/alarm/alert_control.c
@@ -0,0 +1,29 @@
+#include "alert_control.h"
+
+void alert_control_init() {
+    // Initialization code for alert control system
+}
+
+void turn_on_red_LED() {
+    // Code to turn on solid red LED
+}
+
+void turn_on_green_LED() {
+    // Code to turn on solid green LED
+}
+
+void turn_on_blue_LED() {
+    // Code to turn on solid blue LED
+}
+
+void red_LED_breathe() {
+    // Code to make red LED breathe
+}
+
+void red_LED_flash() {
+    // Code to make red LED flash
+}
+
+void stop_LED_effects() {
+    // Code to stop any ongoing LED effects
+}
