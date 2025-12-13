a68aaa600827f3fe6dc4ab3f9578f5cbe9d4fbfa
ElliceNels <23371323@studentmail.ul.ie>
2025-12-13 22:42:48 +0000

    [Feat] Add alert control task

---
+++ Alarm-System-Workspace/m4/src/alert_control.h
@@ -0,0 +1,17 @@
+#ifndef ALERT_CONTROL_H
+#define ALERT_CONTROL_H
+
+#include "typing.h"
+
+// -> add cloud update event to cloud queue to be processed
+int send_cloud_update(cloud_update_event* update);
+
+/* Alert Control Task:
+-> Monitors state machine
+-> Processes events from motion and command queues
+-> Applies physical alerts
+-> Sends cloud update events
+*/
+void AlertControlTask(void *arg);
+
+#endif /* ALERT_CONTROL_H */
